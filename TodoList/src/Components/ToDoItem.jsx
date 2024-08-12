62353c2d08978f937e829e4e5996e8ca4e4e68c5
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-08-12 09:25:43 +0530

    final_commit for today

--- TodoList/src/Components/ToDoItem.jsx
+++ TodoList/src/Components/ToDoItem.jsx
@@ -1,11 +1,7 @@
 function ToDoItem()
 {
     return (<>
-<<<<<<< HEAD
         <h2>Heloo i am child of toDo  AKA "ToDoItems"</h2>
-=======
-        <h2>Heloo i am child of toDo  AKA "ToDoItem"</h2>
->>>>>>> 34016479e9011068f715b27bdc59ec1a745e1ff5
     </>)
 }
 
